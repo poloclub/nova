7c1037bec1963062744e89693a1d9a29099c543c
Jay Wang <jay@zijie.wang>
2022-05-07 09:10:40 -0700

    Bump version: 0.1.1 → 0.1.2

--- svelte/notebook-widget/setup.py
+++ svelte/notebook-widget/setup.py
@@ -49,6 +49,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/poloclub/nova",
-    version="0.1.1",
+    version="0.1.2",
     zip_safe=False,
 )
